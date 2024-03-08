190701da5281516613de3f098594536f2b65074d
KarlGerard <karl.gerard@orange.fr>
2024-03-08 19:17:52 +0100

    v3 -tentative de soket

--- script.js
+++ script.js
@@ -2,7 +2,26 @@ const character = document.getElementById('character');
 const advanceButton = document.getElementById('avance');
 const retreatButton = document.getElementById('recule');
 
-
+/*document.addEventListener('DOMContentLoaded', function() {
+  const welcomeSound = document.getElementById('welcomeSound');
+  
+  if (welcomeSound) {
+    // Jouer la musique
+    welcomeSound.play();
+    
+    // Arrêter la lecture après 3 secondes
+    setTimeout(function() {
+      welcomeSound.pause();
+      welcomeSound.currentTime = 0; // Revenir au début de la piste
+    }, 2500); // Durée en millisecondes
+    
+    // Démarre le fondu en sortie après 2 secondes (temps de lecture)
+    setTimeout(function() {
+      // Utilise jQuery pour effectuer le fondu en sortie sur une seconde
+      $(welcomeSound).fadeOut(200);
+    }, 2000); // 2 secondes (2000 millisecondes)
+  }
+});*/
 
 // Coordonnées de chaque étape du parcours en zigzag
 const moves = [
@@ -41,3 +60,18 @@ function moveCharacter() {
   character.style.left = x;
   character.style.bottom = y;
 }
+
+
+const socket = io();
+
+// Exemple de gestion d'un clic sur un bouton
+document.getElementById('avance').addEventListener('click', () => {
+  // Effectuez l'action utilisateur ici
+  // Par exemple, envoyer une interaction au serveur
+  socket.emit('interaction', { action: 'avancer' });
+});
+
+// Écoutez les interactions provenant d'autres utilisateurs
+socket.on('interaction', (data) => {
+  // Traitez l'interaction reçue ici
+});
